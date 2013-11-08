b807771a8893e5c816f4017e74883f6a1d8fc8a8
Andrew Hobden <andrew@hoverbear.org>
2013-11-08 14:28:26 -0800

    Added packet parsing.

--- Assignment 2/rdps.c
+++ Assignment 2/rdps.c
@@ -28,12 +28,13 @@
 ///////////////////////
 // A parsed (Or not yet rendered) packet.
 typedef struct packet {
-  char*       magic;    // CSc361 RDP protocol.
-  char*       type;     // Byte sequence number.
+  // char*       magic;    // CSc361 RDP protocol. Not needed, if they don't send this, exit.
+  char*       type;     // Type of packet.
+  int         ackno;    // Byte sequence number.
   int         seqno;    // Byte acknowledgement number.
   int         payload;  // Payload Length in bytes.
   int         size;     // Window Size in bytes.
-                      // Empty line goes here.
+                        // Empty line goes here.
   char*       data;     // The data of the payload.
 } packet;
 
@@ -60,7 +61,7 @@ FILE          sender_file;
 // Render a packet based on the struct given.
 char* render_packet(packet source) {
   char* result = calloc( 1024, sizeof(char) );
-  if (result == NULL) { fprintf(stderr, "Couldn't render a packet."); exit(-1); }
+  if (result == NULL) { fprintf(stderr, "Couldn't render a packet.\n"); exit(-1); }
   // TODO
   return result;
 }
@@ -68,8 +69,62 @@ char* render_packet(packet source) {
 // Parse out a packet into a struct given the data.
 packet* parse_packet(char* source) {
   packet* result = calloc( 1, sizeof(struct packet) );
-  if (result == NULL) { fprintf(stderr, "Couldn't parse a packet."); exit(-1); }
-  // TODO
+  if (result == NULL) { fprintf(stderr, "Couldn't parse a packet.\n"); exit(-1); }
+  // Check for the _magic_, move along if satisfied.
+  if (strncmp(source, "_magic_ CSc361\r\n", 16) != 0) { fprintf(stderr, "Bad packet magic... Exiting.\n"); exit(-1); }
+  source = &source[16]; //16 to cut off the \r\n 
+  // Check the _type_.
+  if (strncmp(source, "_type_ ", 7) != 0) { fprintf(stderr, "Bad packet type... Exiting.\n"); exit(-1); }
+  source = &source[7]; // Get the space too. 
+  result->type = calloc( 3, sizeof(char) );
+  if (result == NULL) { fprintf(stderr, "Couldn't set type of a packet.\n"); exit(-1); }
+  strncpy(result->type, source, 3);
+  source = &source[3+2]; // Jump to next line;
+  // Check _seqno__.
+  if (strncmp(source, "_seqno_ ", 8) != 0) { fprintf(stderr, "Bad packet seqno... Exiting.\n"); exit(-1); }
+  source = &source[8];
+  int end_of_seq = 0;
+  while (source[end_of_seq] != '\r') { end_of_seq++; }
+  char* temp_string = calloc( end_of_seq + 1, sizeof(char) );
+  if (temp_string == NULL) { fprintf(stderr, "Couldn't parse a packet.\n"); exit(-1); }
+  result->seqno = atoi(temp_string);
+  free(temp_string);
+  source = &source[end_of_seq + 2]; // Jump lines.
+  // Check _ackno_.
+  if (strncmp(source, "_ackno_ ", 8) != 0) { fprintf(stderr, "Bad packet ackno... Exiting.\n"); exit(-1); }
+  source = &source[8];
+  while (source[end_of_seq] != '\r') { end_of_seq++; }
+  temp_string = calloc( end_of_seq + 1, sizeof(char) );
+  if (temp_string == NULL) { fprintf(stderr, "Couldn't parse a packet.\n"); exit(-1); }
+  result->ackno = atoi(temp_string);
+  free(temp_string);
+  source = &source[end_of_seq + 2]; // Jump lines.
+  // Check _length_.
+  if (strncmp(source, "_length_ ", 9) != 0) { fprintf(stderr, "Bad packet length... Exiting.\n"); exit(-1); }
+  source = &source[9];
+  while (source[end_of_seq] != '\r') { end_of_seq++; }
+  temp_string = calloc( end_of_seq + 1, sizeof(char) );
+  if (temp_string == NULL) { fprintf(stderr, "Couldn't parse a packet.\n"); exit(-1); }
+  result->ackno = atoi(temp_string);
+  free(temp_string);
+  source = &source[end_of_seq + 2]; // Jump lines.
+  // Check _size_.
+  if (strncmp(source, "_size_ ", 7) != 0) { fprintf(stderr, "Bad packet size... Exiting.\n"); exit(-1); }
+  source = &source[7];
+  while (source[end_of_seq] != '\r') { end_of_seq++; }
+  temp_string = calloc( end_of_seq + 1, sizeof(char) );
+  if (temp_string == NULL) { fprintf(stderr, "Couldn't parse a packet.\n"); exit(-1); }
+  result->ackno = atoi(temp_string);
+  free(temp_string);
+  source = &source[end_of_seq + 2]; // Jump lines.
+  // Verify there is an empty line.
+  if (strncmp(source, "\r\n", 2) != 0) { fprintf(stderr, "Bad empty line... Exiting.\n"); exit(-1); }
+  // Check data.
+  source = &source[2];
+  result->data = calloc( strlen(source) + 1, sizeof(char) );
+  if (result->data == NULL) { fprintf(stderr, "Couldn't parse a packet.\n"); exit(-1); }
+  strcpy(result->data, source);
+  // Return the data.
   return result;
 }
 
@@ -93,7 +148,7 @@ int main(int argc, char* argv[]) {
   socket_address.sin_port       = htons(sender_port);
   socket_address.sin_addr.s_addr  = inet_addr(sender_ip);
   // Connect.
-  char* test_packet_string = "_magic_ CSc361\r\n_type_ SYN\r\n_seqno_ 1\r\n_length_ 1\r\n_size_ 1\r\n\r\nSome Data.";
+  char* test_packet_string = "_magic_ CSc361\r\n_type_ SYN\r\n_seqno_ 1\r\n_ackno_ 10\r\n_length_ 100\r\n_size_ 1000\r\n\r\nSome Data.";
   packet* test_packet_struct = parse_packet(test_packet_string);
   char* test_packet_result = render_packet(*test_packet_struct);
   // Spawn workers on new connections, dispatch on existing.
